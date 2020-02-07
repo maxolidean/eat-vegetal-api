4ac71a63bfe0f88a947173271aeec874ff9ac221
maxolidean <sergio.borromei@gmail.com>
2020-02-07 16:54:38 -0300

    store recognized and unrecognized vegetables into azure store

--- src/__tests__/routes/v1/upload/post.test.js
+++ src/__tests__/routes/v1/upload/post.test.js
@@ -1,11 +1,19 @@
 const { api } = require('../../config.test');
 const { PATH_BASE } = require('./const.test');
 
-describe('POST', () => {
-    it('should upload an image by using Multipart', () => 
-      api.post(PATH_BASE)
-      .set('Content-Type', 'multipart/form-data')
-      .attach('image', './src/__tests__/routes/v1/upload/frutillas.jpg')
-      .expect(202));
-});
+// describe('POST', () => {
+//     it('should upload an image by using Multipart', () => 
+//       api.post(PATH_BASE)
+//       .set('Content-Type', 'multipart/form-data')
+//       .attach('image', './src/__tests__/routes/v1/upload/frutillas.jpg')
+//       .expect(202));
+// });
   
+describe('POST', () => {
+  it('should upload an image by using Multipart', () => 
+    api.post(PATH_BASE)
+    .set('Content-Type', 'multipart/form-data')
+    .attach('image', './src/__tests__/routes/v1/upload/berenjenas.jpg')
+    .expect(202));
+});
+
--- src/routes/v1/upload/post.js
+++ src/routes/v1/upload/post.js
@@ -1,35 +1,57 @@
 const { InternalServerError } = require('restify-errors');
 const router = require('./router');
 const fs = require('fs');
+const util = require('util');
 const path = require('path')
 const uniqueFilename = require('unique-filename');
 const { CUSTOM_VISION_ENDPOINT, 
         CUSTOM_VISION_SUBSCRIPTION_KEY, 
         CUSTOM_VISION_PROJECT_ID,
         CUSTOM_VISION_ITERATION_ID,
+        CUSTOM_VISION_MIN_PROB,
         AZURE_STORAGE_CONNECTION_STRING,
-        AZURE_STORAGE_CONTAINER_NAME } = process.env;
-const util = require('util');
+        AZURE_STORAGE_CONTAINER_NAME_UNKNOWN,
+        AZURE_STORAGE_CONTAINER_NAME_RECOGNIZED } = process.env;
 const { PredictionAPIClient } = require("@azure/cognitiveservices-customvision-prediction");
 const { BlobServiceClient } = require("@azure/storage-blob"); 
 const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-const containerClient = blobServiceClient.getContainerClient(AZURE_STORAGE_CONTAINER_NAME);
-const getStream = require('into-stream');
+const unknownContainerClient = blobServiceClient.getContainerClient(AZURE_STORAGE_CONTAINER_NAME_UNKNOWN);
+const recognizedContainerClient = blobServiceClient.getContainerClient(AZURE_STORAGE_CONTAINER_NAME_RECOGNIZED);
 
 
 router.post('', async (req, res, next) => {
   try 
   {
     if (req.files.image != undefined ) {
-      let fileName = uniqueFilename('') + path.extname(req.files.image.name);
-      fs.renameSync(req.files.image.path,  "./uploads/in" + fileName );
-      const testFile = fs.readFileSync("./uploads/in" + fileName);
+      let unq = uniqueFilename('');
+      let fileName = unq + path.extname(req.files.image.name);
+      let resName = unq + ".json";
+      
+      fs.renameSync(req.files.image.path,  "./uploads/" + fileName );
+      const testFile = fs.readFileSync("./uploads/" + fileName);
       const predictor = new PredictionAPIClient(CUSTOM_VISION_SUBSCRIPTION_KEY, CUSTOM_VISION_ENDPOINT);
       const results = await predictor.classifyImage(CUSTOM_VISION_PROJECT_ID, CUSTOM_VISION_ITERATION_ID, testFile);
       res.send(202, {message: 'File processed', results: results });
 
-      const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-      const uploadResp = await blockBlobClient.upload(testFile.buffer,  testFile.length);
+      fs.writeFileSync("./uploads/" + resName, JSON.stringify(results), 'utf-8');
+      const resultsFile = fs.readFileSync("./uploads/" + resName);
+
+      var block, block2;
+      if (results.predictions[0].probability >= parseFloat(CUSTOM_VISION_MIN_PROB)) {
+        block = recognizedContainerClient.getBlockBlobClient(fileName);
+        block2 = recognizedContainerClient.getBlockBlobClient(resName);
+      }
+      else {
+        block = unknownContainerClient.getBlockBlobClient(fileName);
+        block2 = unknownContainerClient.getBlockBlobClient(resName);
+      }
+      
+      await block2.uploadFile("./uploads/" + resName);
+      await block.uploadFile("./uploads/" + fileName);
+
+      fs.unlinkSync("./uploads/" + resName);
+      fs.unlinkSync("./uploads/" + fileName);
+
     }
     else {
       res.send(400, {message: 'Bad Request'});
